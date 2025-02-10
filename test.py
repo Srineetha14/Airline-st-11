e7416a34f186352be1e36aa6adee9b8ef7cbeb5d
KRISHNA VARDHAN <kittu16112004@gmail.com>
2025-02-10 10:24:06 +0530

    asdfghj

--- test.py
+++ test.py
@@ -1,27 +1,38 @@
 import re
-from string import capwords
 import nltk
-from nltk.corpus import word_tokenize
+from nltk.tokenize import word_tokenize
+nltk.download("punkt_tab")
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from nltk.stem import PorterStemmer
 
 
-class Texttonum:
-    def __init__(self):
-        pass
+class TextToNum:
+    def __init__(self,text):
+        self.text=text
 
     def cleaner(self):
-        text = re.sub(r',', '', self.test)
-        cleaned_text = re.sub(r'[^\w\s]', '', text)
-        cleaned_text = re.sub(r'\s+', ' ', cleaned_text)
-        cleaned_text = cleaned_text.strip()
-        self.cleaned = cleaned_data
+        text = re.sub(r',','',self.text)
+        cleaned_text = re.sub(r'[^\w\s]', '', text)  # Removes everything except word characters and spaces
+        cleaned_text = re.sub(r'\s+', ' ', cleaned_text)  # Replaces multiple spaces with a single space
+        cleaned_data = cleaned_text.strip()  # Removes leading/trailing whitespace
+        self.cleaned=cleaned_data
 
     def token(self):
-        self.tkns = word_tokenize(self.cleaned)
+        self.tkns=word_tokenize(self.cleaned)
 
     def removeStop(self):
-        stop = stopwords.words('english')
-        self.c1 = [i for i in self.tkns if i not in stop]
+        stop=stopwords.words('english')
+        self.cl = [i for i in self.tkns if i not in stop]
 
     def stemme(self):
-        ps = PorterStemmer()
-        self.st = [ps.stem(word)]
+        ps=PorterStemmer()
+        self.st = [ps.stem(word) for word in self.cl]
+        return self.st
+    
+
+    
+
+
+
+    
